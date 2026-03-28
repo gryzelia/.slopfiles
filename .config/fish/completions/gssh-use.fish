e28e4e9fcecbf5f0143517d80406a381d1760de9
gryzelia <gryzelia@tutamail.com>
2026-03-28 01:20:39 -0700

    fish: remove outdated gssh scripts

--- .config/fish/completions/gssh-use.fish
+++
@@ -1,4 +0,0 @@
-complete -c gssh-use -f
-complete -c gssh-use -s g -l global -d "Set globally instead of for the current repo"
-complete -c gssh-use -n "test (count (commandline -opc)) -eq 1" \
-    -a "(nu -c 'use ~/.config/fish/nu/gssh.nu *; list-usernames' 2>/dev/null)"
